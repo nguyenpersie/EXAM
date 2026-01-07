0f8ade9985446c650a3ff35450bde75f469d9a2e
nguyenpersie <nguyenpersie27@gmail.com>
2026-01-07 08:52:27 +0700

    init

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/',)
+Route::get('/', [HomeController::class, 'index'])->name('pages.home');
