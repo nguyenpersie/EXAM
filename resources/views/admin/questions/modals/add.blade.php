511cebb02d9256607d5f41418f2acfb712161a55
nguyenpersie <nguyenpersie27@gmail.com>
2026-01-12 16:16:46 +0700

    add exam

--- resources/views/admin/questions/modals/add.blade.php
+++ resources/views/admin/questions/modals/add.blade.php
@@ -5,39 +5,37 @@
         <h1 class="modal-title fs-5" id="staticBackdropLabel">Thêm câu hỏi</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-      <div class="modal-body">
-        <form action="{{ route('customer.add.post') }}" method="POST" enctype="multipart/form-data">
-          @csrf
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Tên</label>
-            <input type="text" class="form-control" id="customer_name" name="customer_name">
-        </div>
-        <div  class="mb-3">
-            <label for="floatingInputValue">Gmail</label>
-            <input type="email" class="form-control" id="usermail" name="email" placeholder="@gmail.com">
-        </div>
-        <div class="mb-3">
-            <label for="numberphone" class="form-label">Điện thoại</label>
-            <input type="text" class="form-control" id="tel_num" name="tel_num">
-        </div>
-        <div class="mb-3">
-            <label for="address" class="form-label">Địa chỉ</label>
-            <textarea name="address" id="address" cols="20" rows="3" class="form-control"></textarea>
-        </div>
-        <div class="mb-3 form-check">
-            <label class="form-check-label" for="exampleCheck1">Trạng thái</label>
-            <fieldset class="form-group row">
-              <div class="col-sm-10">
-                <div class="form-check">
-                  <label class="form-check-label col-sm-3">
-                    <input class="form-check-input radio-inline" type="radio" name="is_active" id="is_active" value="1" checked>Yes</label>
-                    <label class="form-check-label">
-                    <input class="form-check-input radio-inline" type="radio" name="is_active" id="is_active" value="2">No</label>
-                </div>
-                </div>
-            </fieldset>
-        </div>
-      </div>
+        <div class="mb-4">
+              <label class="block">Nội dung câu hỏi</label>
+              <textarea name="content" rows="4" class="w-full border p-2"></textarea>
+          </div>
+
+          <div class="mb-4">
+              <label class="block">Phần</label>
+              <select name="section" class="w-full border p-2">
+                  <option value="I">Phần I</option>
+                  <option value="II">Phần II</option>
+                  <option value="III">Phần III</option>
+              </select>
+          </div>
+
+          <div class="mb-4">
+              <label class="block">Độ khó (1-5)</label>
+              <input type="number" name="level" min="1" max="5" value="" class="w-full border p-2">
+          </div>
+
+          <div class="mb-4">
+              <label class="block">Các đáp án (4 đáp án)</label>
+              @for($i = 0; $i < 4; $i++)
+              <div class="flex mb-2">
+                  <input type="text" name="options[{{ $i }}][content]" value="" class="flex-1 border p-2 mr-2">
+                  <label class="flex items-center">
+                      <input type="radio" name="correct_option" value="">
+                      Đúng
+                  </label>
+              </div>
+              @endfor
+          </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Hủy</button>
         <button type="submit" class="btn btn-primary">Lưu</button>
