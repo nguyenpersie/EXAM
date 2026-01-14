ea444916ec8b23c7208d923c8d94422903b68894
nguyenpersie <nguyenpersie27@gmail.com>
2026-01-14 15:00:30 +0700

    udpate model user

--- app/Models/UserExam.php
+++ app/Models/UserExam.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Laravel\Sanctum\HasApiTokens;
 
 class UserExam extends Authenticatable
 {
     protected $primaryKey = 'user_id';
-    use HasApiTokens, Notifiable;
+    use HasApiTokens, Notifiable, HasFactory;
 
     protected $table = 'users_exam';
 
