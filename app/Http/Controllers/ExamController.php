ff167fe8f658ba5f216abfbfeb893e81fe8227fb
nguyenpersie <nguyenpersie27@gmail.com>
2026-01-15 09:42:28 +0700

    update exam controller

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -9,7 +9,7 @@ class ExamController extends Controller
 {
     public function test($id)
     {
-        $exam = Exam::with('questions.options')->findOrFail($id);
+        $exam = Exam::with('questions.options')->findOrFail(67);
         return view('pages.test', compact('exam'));
     }
 }
