2dc2bdae5b418da1c817c91ba08216f37065eecc
nguyenpersie <nguyenpersie27@gmail.com>
2026-01-15 14:08:58 +0700

    update usercontroller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class UserController extends Controller
 {
-    //
+    public function login(): View
+    {
+        return view ('pages.home');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,14 @@
 use App\Http\Controllers\ExamController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', [HomeController::class, 'index'])->name('pages.home');
+Route::get('/', [UserController::class, 'login'])->name('pages.home');
 Route::resource('questions', QuestionController::class)->names('admin.questions');
 Route::get('/exams-{id}/test', [ExamController::class, 'test'])->name('exams.test');
 
