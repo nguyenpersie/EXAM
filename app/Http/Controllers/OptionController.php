b357af21d810c6d1381cca8166f8e8e776acea08
nguyenpersie <nguyenpersie27@gmail.com>
2026-01-20 15:33:08 +0700

    update option controller

--- app/Http/Controllers/OptionController.php
+++ app/Http/Controllers/OptionController.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Services\OptionService;
 use Illuminate\Http\Request;
 use Illuminate\Contracts\View\View;
 
 class OptionController extends Controller
 {
+
+ protected OptionService $optionService;
+
+    public function __construct(OptionService $optionService)
+    {
+        $this->optionService = $optionService;
+    }
+
     public function index(): View
     {
         $options = $this->optionService->getPaginatedOptions();
