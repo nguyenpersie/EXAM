f982c3e25c30da041b21c72398013ab671bbfbe0
nguyenpersie <nguyenpersie27@gmail.com>
2026-01-28 09:02:19 +0700

    update exam-test.js

--- public/js/exam-test.js
+++ public/js/exam-test.js
@@ -207,7 +207,7 @@ function showResultModal(result) {
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header bg-${result.isPassed ? 'success' : 'danger'} text-white">
-                    <h5><i class="bi bi-${result.isPassed ? 'check-circle' : 'x-circle'}"></i> Kết quả bài thi</h5>
+                    <h5 class="modal-title"><i class="bi bi-${result.isPassed ? 'check-circle' : 'x-circle'}"></i> Kết quả bài thi</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
