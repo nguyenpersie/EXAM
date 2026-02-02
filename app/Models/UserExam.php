ed8bf6bb8ec381302249e999d7e051fe59df3f9f
nguyenpersie <nguyenpersie27@gmail.com>
2026-02-02 16:27:02 +0700

    update userexamcomtroller

--- app/Models/UserExam.php
+++ app/Models/UserExam.php
@@ -5,12 +5,7 @@ namespace App\Models;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Laravel\Sanctum\HasApiTokens;
-
-class UserExam extends Authenticatable
-{
-    protected $primaryKey = 'id';
-    use HasApiTokens, Notifiable, HasFactory;
+    use Notifiable, HasFactory;
 
     protected $table = 'users';
 
