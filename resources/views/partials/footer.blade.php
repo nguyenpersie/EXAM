4049731aa9030453d86d39cc0eb34e4aea5b9b24
nguyenpersie <nguyenpersie27@gmail.com>
2026-02-06 14:02:35 +0700

    update icon master

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,19 +1,15 @@
 <footer class="main-footer">
-      <div class="container-fluid">
-        <div class="row align-items-center">
-          <div class="col-auto">
-            <a href="http://157.66.24.117/">
-              <img
-                src="{{ asset('assets/images/logo-SH.png') }}"
-                class="footer-logo"
-                alt="Logo"
-              />
-            </a>
-          </div>
-          <div class="col">
-            <h6 class="fw-bold mb-1 text-uppercase">Trung Tâm Dạy Nghề Đường Thủy Sông Hậu</h6>
-            <p class="mb-0">D30 Đường số 30 khu ĐTM Hưng Phú, Phường Cái Răng, TP. Cần Thơ | Hotline: 0325207333</p>
-          </div>
-        </div>
+  <div class="container-fluid">
+    <div class="row align-items-center">
+      <div class="col-auto">
+        <a href="http://157.66.24.117/">
+          <img src="{{ asset('assets/images/logo-SH.png') }}" class="footer-logo" alt="Logo" />
+        </a>
       </div>
-    </footer>
+      <div class="col">
+        <h6 class="fw-bold mb-1 text-uppercase">Trung Tâm Dạy Nghề Đường Thủy Sông Hậu</h6>
+        <p class="mb-0">D30 Đường số 30 khu ĐTM Hưng Phú, Phường Cái Răng, TP. Cần Thơ | Hotline: 0325207333</p>
+      </div>
+    </div>
+  </div>
+</footer>
